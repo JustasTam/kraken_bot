94b20128d72caa23f5b218afccae76c1b4d23f85
JustasTam <justas.tamulionis@gmail.com>
2020-05-15 12:42:18 +0300

    removed redundant code from rufus initializer

--- config/initializers/rufus_scheduler.rb
+++ config/initializers/rufus_scheduler.rb
@@ -1,23 +1,7 @@
-# # config/initializers/rufus_scheduler.rb
-# require 'rufus-scheduler'
-# require 'rake'
-# MyRailsApp::Application.load_tasks
-
-# # Let's use the rufus-scheduler singleton
-# #
-# s = Rufus::Scheduler.singleton
-
-# # Awesome recurrent task...
-# #
-# s.every '1m' do
-#   rake 'cron_bot_launcher:launch'
-# end
-
 require 'rufus-scheduler'
 require 'rake'
 KrakenBot::Application.load_tasks
 
-# load File.join(Rails.root, 'lib', 'tasks', 'task_runner.rake')
 s = Rufus::Scheduler.new
 
 s.every '1m' do 
